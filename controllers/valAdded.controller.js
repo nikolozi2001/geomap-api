29d488aeb20e4964aa9cc62dc810e9531b7a7485
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-01-31 17:14:03 +0400

    add valAdded endpoint to API for retrieving regional data by year

---
+++ controllers/valAdded.controller.js
@@ -0,0 +1,18 @@
+const con = require("../config/db.config");
+
+exports.valAdded = (req, res) => {
+  let id = req.query.id.toString();
+  let year = req.query.year.toString();
+
+  con.query(
+    `SELECT OGR_FID, region_id, name_ge, name_en, w_${year} FROM reg_damatebuli_girebuleba WHERE region_id=${id}`,
+    function (err, result) {
+      if (err) {
+        console.error("Query error:", err);
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const regBrunvaController = require("../controllers/regBrunva.controller");
 const payGenderController = require("../controllers/payGender.controller");
+const valAddedController = require("../controllers/valAdded.controller");
 
 router.get("/getRegBrunva", regBrunvaController.getRegBrunva);
 router.get("/getPayGender", payGenderController.getPayGender);
+router.get("/valAdded", valAddedController.valAdded);
 
 module.exports = router;
