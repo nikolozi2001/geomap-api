7f1c13a7516ddc5836f8b86bfaf1c32d9d2ebc2f
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-07-03 16:48:45 +0400

    fix(regEmployeesGender): update SQL query to use lowercase column names for consistency

--- controllers/regEmployeesGender.controller.js
+++ controllers/regEmployeesGender.controller.js
@@ -6,7 +6,7 @@ exports.getRegEmployeesGender = async (req, res) => {
 
   try {
     const [rows] = await con.query(
-      `SELECT REGION_ID, NAME_GE, F_${fyear}, M_${myear} FROM reg_dasaqmeba_sqesit`
+      `SELECT region_id, name_ge, name_en, f_${fyear}, m_${myear} FROM reg_dasaqmeba_sqesit`
     );
     res.json(rows);
   } catch (error) {
